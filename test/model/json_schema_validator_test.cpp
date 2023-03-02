7c8895e1ca6817abd187907026aaa287368897e1
Guanhua Hou <gaimanjing@gmail.com>
2023-03-02 09:07:31 +0800

    added: test case for validating json file

--- test/model/json_schema_validator_test.cpp
+++ test/model/json_schema_validator_test.cpp
@@ -1,9 +1,13 @@
 #include <gtest/gtest.h>
-
+#include <fstream>
 #include "JsonSchemaValidator.hpp"
 
 using json = nlohmann::json;
 using nlohmann::json_schema::json_validator;
+
+void validate(JsonSchemaValidator& sut,
+              const std::string& schema_file_name,
+              const std::string& json_file_name);
 
 class VggJsonSchemaValitatorTestSuite : public ::testing::Test
 {
@@ -56,4 +60,26 @@ TEST_F(VggJsonSchemaValitatorTestSuite, smoke)
   // When && Then
   EXPECT_EQ(false, sut.validate(bad_person));
   EXPECT_EQ(true, sut.validate(good_person));
+}
+
+TEST_F(VggJsonSchemaValitatorTestSuite, withFileName)
+{
+  validate(sut, "./testDataDir/vgg-format.json", "./testDataDir/2020.json");
+}
+
+void validate(JsonSchemaValidator& sut,
+              const std::string& schema_file_name,
+              const std::string& json_file_name)
+{
+  // Given
+  std::ifstream schema_fs(schema_file_name);
+  std::ifstream json_fs(json_file_name);
+
+  json schema = json::parse(schema_fs);
+  json json_data = json::parse(json_fs);
+
+  sut.setRootSchema(schema);
+
+  // When && Then
+  EXPECT_EQ(true, sut.validate(json_data));
 }

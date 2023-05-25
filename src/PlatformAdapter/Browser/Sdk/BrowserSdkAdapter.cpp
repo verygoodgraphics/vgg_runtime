31445abd4f40986669e3e7cd177ec3e9b922ae6c
Guanhua Hou <gaimanjing@gmail.com>
2023-05-25 11:21:38 +0800

    implemented: sdk event listener api

--- src/PlatformAdapter/Browser/Sdk/BrowserSdkAdapter.cpp
+++ src/PlatformAdapter/Browser/Sdk/BrowserSdkAdapter.cpp
@@ -12,5 +12,8 @@ EMSCRIPTEN_BINDINGS(vgg_sdk)
     .function("getDesignDocument", &VggSdk::designDocument)
     .function("addAt", &VggSdk::designDocumentAddAt)
     .function("deleteAt", &VggSdk::designDocumentDeleteAt)
-    .function("updateAt", &VggSdk::designDocumentReplaceAt);
+    .function("updateAt", &VggSdk::designDocumentReplaceAt),
+    .function("addEventListener", &VggSdk::addEventListener),
+    .function("removeEventListener", &VggSdk::removeEventListener),
+    .function("getEventListeners", &VggSdk::getEventListeners);
 }

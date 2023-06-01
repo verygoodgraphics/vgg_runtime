874e897a67bcb16310682b24ec2b0ce048a8a5a2
Guanhua Hou <gaimanjing@gmail.com>
2023-06-01 17:51:22 +0800

    fixed: emscripten build error

--- src/PlatformAdapter/Browser/Sdk/BrowserSdkAdapter.cpp
+++ src/PlatformAdapter/Browser/Sdk/BrowserSdkAdapter.cpp
@@ -12,8 +12,8 @@ EMSCRIPTEN_BINDINGS(vgg_sdk)
     .function("getDesignDocument", &VggSdk::designDocument)
     .function("addAt", &VggSdk::designDocumentAddAt)
     .function("deleteAt", &VggSdk::designDocumentDeleteAt)
-    .function("updateAt", &VggSdk::designDocumentReplaceAt),
-    .function("addEventListener", &VggSdk::addEventListener),
-    .function("removeEventListener", &VggSdk::removeEventListener),
+    .function("updateAt", &VggSdk::designDocumentReplaceAt)
+    .function("addEventListener", &VggSdk::addEventListener)
+    .function("removeEventListener", &VggSdk::removeEventListener)
     .function("getEventListeners", &VggSdk::getEventListeners);
 }

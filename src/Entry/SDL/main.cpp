fcabd36518e1f2752394e2d7fd19262f00dfc3b8
Shuoliu Yang <visysl@outlook.com>
2023-06-07 14:52:32 +0800

    update dir

--- src/Entry/SDL/main.cpp
+++ src/Entry/SDL/main.cpp
@@ -1,24 +1,8 @@
-/*
- * Copyright (C) 2021-2023 Chaoya Li <harry75369@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-#include <argparse/argparse.hpp>
-#include <filesystem>
-#include "Entry/SDL/SDLRuntime.hpp"
+#include "SDLRuntime.hpp"
 #include "Utils/FileManager.hpp"
 #include <memory>
+#include <argparse/argparse.hpp>
+#include <filesystem>
 
 #include <Scene/Scene.h>
 #include <Reader/LoadUtil.hpp>

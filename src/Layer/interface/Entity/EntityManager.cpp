e16ce359a6c9bc63b64990982ee8666c081ec22b
Shuoliu Yang <visysl@outlook.com>
2023-06-08 15:34:27 +0800

    fixup macos compilation

--- src/Layer/interface/Entity/EntityManager.cpp
+++ src/Layer/interface/Entity/EntityManager.cpp
@@ -14,8 +14,6 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_keyboard.h>
 
 #include "Entity/EntityManager.hpp"
 #include "Entity/MouseEntity.hpp"

be39e69ef4ae2c273c12ee7936e2fad8208abf6c
Guanhua Hou <gaimanjing@gmail.com>
2023-06-16 09:29:21 +0800

    added: zip to dir/zip unit tests

--- test/editor/save_test.cpp
+++ test/editor/save_test.cpp
@@ -16,6 +16,7 @@ constexpr auto event_listeners_file_name = "event_listeners.json";
 constexpr auto layout_file_name = "layout.json";
 
 constexpr auto model_src_dir_path = "testDataDir/vgg-work/";
+constexpr auto model_src_zip_path = "testDataDir/vgg-work.zip";
 
 class EditorSaveTestSuite : public ::testing::Test
 {
@@ -36,44 +37,74 @@ protected:
   void TearDown() override
   {
   }
+
+  void load_from_dir()
+  {
+    // Given
+    std::string path{ model_src_dir_path };
+    m_model->load(path);
+  }
+
+  void load_from_zip()
+  {
+    // Given
+    std::string path{ model_src_zip_path };
+    m_model->load(path);
+  }
+
+  void save_to_dir()
+  {
+    std::string dst_dir{ "tmp/" };
+    auto saver{ std::make_shared<Model::DirSaver>(dst_dir) };
+    Editor sut{ m_model, saver };
+
+    // When
+    sut.save();
+
+    // Then
+    std::filesystem::path dir{ dst_dir };
+    auto ret = fs::is_regular_file(dir / artboard_file_name);
+    EXPECT_TRUE(ret);
+
+    ret = fs::is_regular_file(dir / event_listeners_file_name);
+    EXPECT_TRUE(ret);
+  }
+
+  void save_to_zip()
+  {
+    std::string dst_file{ "tmp/1.zip" };
+    auto saver{ std::make_shared<Model::ZipSaver>(dst_file) };
+    Editor sut{ m_model, saver };
+
+    // When
+    sut.save();
+
+    // Then
+    auto ret = fs::is_regular_file(dst_file);
+    EXPECT_TRUE(ret);
+  }
 };
 
 TEST_F(EditorSaveTestSuite, dir_to_dir)
 {
-  // Given
-  std::string path{ model_src_dir_path };
-  m_model->load(path);
-
-  std::string dst_dir{ "tmp/" };
-  auto saver{ std::make_shared<Model::DirSaver>(dst_dir) };
-  Editor sut{ m_model, saver };
-
-  // When
-  sut.save();
-
-  // Then
-  std::filesystem::path dir{ dst_dir };
-  auto ret = fs::is_regular_file(dir / artboard_file_name);
-  EXPECT_TRUE(ret);
-
-  ret = fs::is_regular_file(dir / event_listeners_file_name);
-  EXPECT_TRUE(ret);
+  load_from_dir();
+  save_to_dir();
 }
 
 TEST_F(EditorSaveTestSuite, dir_to_zip)
 {
-  // Given
-  std::string path{ model_src_dir_path };
-  m_model->load(path);
+  load_from_dir();
+  save_to_zip();
+}
 
-  std::string dst_file{ "tmp/1.zip" };
-  auto saver{ std::make_shared<Model::ZipSaver>(dst_file) };
-  Editor sut{ m_model, saver };
+TEST_F(EditorSaveTestSuite, zip_to_dir)
+{
+  load_from_zip();
+  save_to_dir();
+}
 
-  // When
-  sut.save();
-
-  // Then
-  auto ret = fs::is_regular_file(dst_file);
-  EXPECT_TRUE(ret);
+TEST_F(EditorSaveTestSuite, zip_to_zip)
+{
+  load_from_zip();
+  save_to_zip();
 }

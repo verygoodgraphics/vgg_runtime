26a881b99db45c4a01524be4014a56c95a3530d9
Guanhua Hou <gaimanjing@gmail.com>
2023-08-01 10:27:00 +0800

    updated: observer edit model to update edit view

--- src/Application/include/Application/Presenter.hpp
+++ src/Application/include/Application/Presenter.hpp
@@ -2,6 +2,7 @@
 
 #include "Domain/ModelEvent.hpp"
 #include "Domain/Daruma.hpp"
+#include "Log.h"
 #include "UIEvent.hpp"
 #include "UIView.hpp"
 
@@ -23,6 +24,7 @@ class Presenter : public std::enable_shared_from_this<Presenter>
 
   rxcpp::subjects::subject<VGG::UIEventPtr> m_subject;
   rxcpp::observer<VGG::ModelEventPtr> m_model_observer;
+  rxcpp::observer<VGG::ModelEventPtr> m_edit_model_observer;
 
 public:
   virtual ~Presenter() = default;
@@ -31,14 +33,21 @@ public:
   {
     m_model = model;
     m_view->setResouces(m_model->resources());
-    show();
+    m_view->show(m_model->designDoc()->content());
   }
 
   void setEditModel(std::shared_ptr<Daruma> model)
   {
     m_edit_model = model;
-    m_edit_view->setResouces(m_edit_model->resources());
-    m_edit_view->show(m_edit_model->designDoc()->content());
+    if (m_edit_view)
+    {
+      m_edit_view->setResouces(m_edit_model->resources());
+      m_edit_view->show(m_edit_model->designDoc()->content());
+    }
+    else
+    {
+      WARN("Presenter::setEditModel: empty m_edit_view");
+    }
   }
 
   void setView(std::shared_ptr<UIView> view)
@@ -101,20 +110,63 @@ public:
     return m_model_observer;
   }
 
+  virtual rxcpp::observer<VGG::ModelEventPtr>& getEditModelObserver()
+  {
+    auto weak_this = weak_from_this();
+    m_edit_model_observer = rxcpp::make_observer_dynamic<ModelEventPtr>(
+      [weak_this](ModelEventPtr event)
+      {
+        switch (event->type)
+        {
+          case ModelEventType::Add:
+          {
+            auto add_event_ptr = static_cast<ModelEventAdd*>(event.get());
+          }
+          break;
+
+          case ModelEventType::Delete:
+          {
+            auto delete_event_ptr = static_cast<ModelEventDelete*>(event.get());
+          }
+          break;
+
+          case ModelEventType::Update:
+          {
+            auto udpate_event_ptr = static_cast<ModelEventUpdate*>(event.get());
+          }
+          break;
+
+          default:
+            break;
+        }
+
+        if (auto p = weak_this.lock())
+        {
+          // todo, diff & update
+          p->update_edit_view();
+        }
+      });
+
+    return m_edit_model_observer;
+  }
+
   virtual rxcpp::observable<VGG::UIEventPtr> getObservable()
   {
     return m_subject.get_observable();
   }
 
 private:
-  void show()
+  void update()
   {
     m_view->show(m_model->designDoc()->content());
   }
 
-  void update()
+  void update_edit_view()
   {
-    m_view->show(m_model->designDoc()->content());
+    if (m_edit_view && m_edit_model)
+    {
+      m_edit_view->show(m_edit_model->designDoc()->content());
+    }
   }
 };
 

4ba8f2c6ba9ce6a6d4209b3ddb0dffd0bed10365
Guanhua Hou <gaimanjing@gmail.com>
2023-08-11 11:38:01 +0800

    added: ExpandSymbol internal method

--- src/Domain/include/Domain/Layout/ExpandSymbol.hpp
+++ src/Domain/include/Domain/Layout/ExpandSymbol.hpp
@@ -26,6 +26,11 @@ public:
 private:
   void collect_master(const nlohmann::json& json);
   void expand_instance(nlohmann::json& json);
+  void scale_from_master(nlohmann::json& instance, nlohmann::json& master);
+  void normalize_children_geometry_within_master(nlohmann::json& instance, nlohmann::json& master);
+  void recalculate_intance_children_geometry(nlohmann::json& instance, nlohmann::json& master);
+  void handle_override(nlohmann::json& instance, nlohmann::json& master);
+  void handle_override_master_id(nlohmann::json& instance, nlohmann::json& master);
 };
 } // namespace Layout
 

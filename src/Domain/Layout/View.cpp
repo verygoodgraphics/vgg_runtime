5250cbb6d0a504fb436b7994950fdd38b0adcdde
Guanhua Hou <gaimanjing@gmail.com>
2023-08-24 17:22:36 +0800

    implemented: apply flex layout result to view

--- src/Domain/Layout/View.cpp
+++ src/Domain/Layout/View.cpp
@@ -5,19 +5,35 @@
 namespace VGG
 {
 
-void LayoutView::configureLayout(const Layout::Internal::ConfigureLayoutFn& configureFn)
+std::shared_ptr<Layout::Internal::Bridge> LayoutView::configureLayout()
 {
-  if (configureFn)
-  {
-    m_bridge.reset(new Layout::Internal::Bridge);
-    m_bridge->view = weak_from_this();
+  m_bridge.reset(new Layout::Internal::Bridge);
+  m_bridge->view = weak_from_this();
 
-    configureFn(m_bridge);
-  }
+  return m_bridge;
+}
+
+std::shared_ptr<flexbox_node> LayoutView::createFlexboxNode()
+{
+  flexbox_node::p_node node{ new flexbox_node };
+  m_bridge->flexNode = node;
+
+  return m_bridge->flexNode;
+}
+
+std::shared_ptr<grid_layout> LayoutView::createGridNode()
+{
+  // todo
+  return m_bridge->gridNode;
 }
 
 void LayoutView::applyLayout()
 {
+  for (auto subview : m_children)
+  {
+    subview->applyLayout();
+  }
+
   if (m_bridge)
   {
     m_bridge->applyLayout(false);

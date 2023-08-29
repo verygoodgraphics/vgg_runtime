85522115fda2239519c056e91ba4dddff5fedf1d
Shuoliu Yang <visysl@outlook.com>
2023-08-29 11:25:33 +0800

    fixup lambda multiple definition

--- test/domain/model/daruma_helper.hpp
+++ test/domain/model/daruma_helper.hpp
@@ -1,14 +1,15 @@
+#pragma once
 #include "Domain/RawJsonDocument.hpp"
 
 namespace Helper
 {
 
-auto RawJsonDocumentBuilder = [](const json& design_json)
+inline auto RawJsonDocumentBuilder(const json& design_json)
 {
   auto raw_json_doc = new RawJsonDocument();
   raw_json_doc->setContent(design_json);
   return JsonDocumentPtr(raw_json_doc);
-};
+}
 
 nlohmann::json load_json(const std::string& json_file_name);
 void write_json(const nlohmann::json& json, const std::string& json_file_name);

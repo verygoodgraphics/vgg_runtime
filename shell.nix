de88388d2f6c21a1a880f6cc679560baeb541e6d
Chaoya Li <harry75369@gmail.com>
2023-09-07 16:02:27 +0800

    updated nix shell for vulkan env

--- shell.nix
+++ shell.nix
@@ -1,7 +1,36 @@
 # NOTE It is for linux deployment build only. Use it with `nix-shell --pure`
-{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz") {}}:
+# NOTE if you want to use vulkan driver, you must be in the "video" user group
+{ pkgs ? import (fetchTarball "https://s5.vgg.cool/nixos-23.05.tar.gz") {}
+, nixgl ? import (fetchTarball "https://github.com/guibou/nixGL/archive/master.tar.gz") {}
+, nvidiaGpu ? false
+}:
 with pkgs;
   mkShell {
-    nativeBuildInputs = [clang cmake ninja python3];
-    buildInputs = [libGL SDL2];
+    buildInputs = [
+      # build tools
+      clang
+      cmake
+      ninja
+      python3
+      glslang # or shaderc
+
+      # libs
+      SDL2
+      vulkan-headers
+      vulkan-loader
+      nixgl.nixGLIntel
+      nixgl.nixVulkanIntel
+
+      # other tools
+      vim
+      less
+      which
+      vulkan-tools # for vulkaninfo
+      mesa-demos   # for eglinfo
+    ]
+    ++ lib.optionals nvidiaGpu [
+      nixgl.auto.nixGLDefault
+      nixgl.auto.nixVulkanNvidia
+    ]
+    ;
   }

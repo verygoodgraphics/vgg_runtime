34bdbd6fc0fff1d83e14aa9487b1c3c11a4565c5
Chaoya Li <harry75369@gmail.com>
2023-09-17 19:09:43 +0800

    make sure to use clang in nix shell; using latest unstable nixpkgs to prevent compiler bugs

--- shell.nix
+++ shell.nix
@@ -1,15 +1,16 @@
 # NOTE It is for linux deployment build only. Use it with `nix-shell --pure`
-# NOTE if you want to use vulkan driver, you must be in the "video" user group
-{ pkgs ? import (fetchTarball "https://s5.vgg.cool/nixos-23.05.tar.gz") {}
-, nixgl ? import (fetchTarball "https://github.com/guibou/nixGL/archive/master.tar.gz") {}
+# NOTE if you failed to run vulkan, please check you are in the "video" user group
+{ pkgs ? import (fetchTarball "https://github.com/verygoodgraphics/nixpkgs/archive/refs/tags/unstable-20230917.tar.gz") {}
+, nixgl ? import (fetchTarball "https://github.com/verygoodgraphics/nixGL/archive/master.tar.gz") {}
 , nvidiaGpu ? false
 }:
 with pkgs;
   mkShell {
     buildInputs = [
       # build tools
-      clang
+      clang_15
       cmake
+      cmakeCurses
       ninja
       python3
       glslang # or shaderc
@@ -33,4 +34,9 @@ with pkgs;
       nixgl.auto.nixVulkanNvidia
     ]
     ;
+
+    shellHook = ''
+      export CC=${clang_15}/bin/clang
+      export CXX=${clang_15}/bin/clang++
+    '';
   }

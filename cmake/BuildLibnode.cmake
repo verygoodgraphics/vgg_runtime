1579f638664b0e12a97678d96fb2b1b0e36722ef
Chaoya Li <harry75369@gmail.com>
2023-09-19 10:41:24 +0800

    improved node building script

--- cmake/BuildLibnode.cmake
+++ cmake/BuildLibnode.cmake
@@ -1,30 +1,31 @@
-  function(build_libnode WORK_DIR)
-    find_library(NODE_LIBRARY_PATH node
-      PATHS "${WORK_DIR}/out/Release"
-      NO_DEFAULT_PATH)
+function(build_libnode WORK_DIR)
 
-    if(NOT NODE_LIBRARY_PATH)
-      message("build libnode...")
+  execute_process(
+    COMMAND ninja -C "${WORK_DIR}/out/Release"
+    ERROR_VARIABLE IS_NOT_BUILDABLE
+  )
 
-      # Configure
-      execute_process(
-        COMMAND ./configure --enable-static --ninja
-        WORKING_DIRECTORY ${WORK_DIR}
-        COMMAND_ERROR_IS_FATAL ANY)
+  if (IS_NOT_BUILDABLE)
+    # Configure
+    execute_process(
+      COMMAND ./configure --enable-static --ninja
+      WORKING_DIRECTORY ${WORK_DIR}
+      COMMAND_ERROR_IS_FATAL ANY)
 
-      # This is a patch if there is only python3 in search path, which is neccessary for building node with ninja
-      execute_process(
-        COMMAND sed -i".bak" "s/env python$/env python3/" tools/specialize_node_d.py
-        WORKING_DIRECTORY "${WORK_DIR}"
-        COMMAND_ERROR_IS_FATAL ANY)
+    # This is a patch if there is only python3 in search path, which is neccessary for building node with ninja
+    execute_process(
+      COMMAND sed -i".bak" "s/env python$/env python3/" tools/specialize_node_d.py
+      WORKING_DIRECTORY "${WORK_DIR}"
+      COMMAND_ERROR_IS_FATAL ANY)
 
-      # Build node with ninja
-      execute_process(
-        COMMAND ninja
-        WORKING_DIRECTORY "${WORK_DIR}/out/Release"
-        COMMAND_ERROR_IS_FATAL ANY)
-    endif()
+    # Build node with ninja
+    execute_process(
+      COMMAND ninja -C "${WORK_DIR}/out/Release"
+      COMMAND_ERROR_IS_FATAL ANY)
+  else()
+    message(STATUS "Node already build.")
+  endif()
 
-  endfunction()
+endfunction()
 
-  build_libnode(${NODE_DIR})
+build_libnode(${NODE_DIR})

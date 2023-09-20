d6fe5bf4aeba689b8041173c374db712941674e9
Shuoliu Yang <visysl@outlook.com>
2023-09-20 15:57:41 +0800

    remove old comments

--- src/Entry/Exporter/interface/ImageExporter.hpp
+++ src/Entry/Exporter/interface/ImageExporter.hpp
@@ -6,28 +6,13 @@
 #include <map>
 #include <nlohmann/json.hpp>
 
-/**
- * @brief
- *
- * @param j vgg_format json object
- * @param resources blob resources map
- * @param imageQuality 0 - 100, 100 is the best quality of the generated image
- * @param resolutionLevel
- * @param configFile specify the config file
- * @param fontCollectionName specify the fontCollectionName given in configFile
- * @return std::tuple<std::string, std::map<int, sk_sp<SkData>>>
- *
- * first element is reason string, error ocurrs if string is not empty.
- * the second element is the render result, the key is the index of the artboard,
- * the value is the binary image data, which format is PNG.
- */
 namespace VGG::exporter
 {
 
 struct ImageOption
 {
   int imageQuality = 100;
-  int resolutionLevel = 100;
+  int resolutionLevel = 2;
 };
 
 enum class EBackend
@@ -71,6 +56,7 @@ public:
 
   Exporter();
   void info(ExporterInfo* info);
+
   Iterator render(nlohmann::json j,
                   std::map<std::string, std::vector<char>> resources,
                   const ImageOption& opt)

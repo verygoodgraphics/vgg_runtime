b54359c9737ddf4603a3e34e6662c81998cddb27
Chaoya Li <harry75369@gmail.com>
2023-10-12 14:17:06 +0800

    added license notice inside source code

--- src/Entry/SDL/AppSDLImpl.hpp
+++ src/Entry/SDL/AppSDLImpl.hpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 VeryGoodGraphics LTD <bd@verygoodgraphics.com>
+ *
+ * Licensed under the VGG License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.verygoodgraphics.com/licenses/LICENSE-1.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include "EventAPISDLImpl.hpp"
@@ -35,6 +50,21 @@ class AppSDLImpl : public AppBase<AppSDLImpl>
   using Setter = std::function<void(std::any)>;
   std::unordered_map<std::string, std::pair<Getter, Setter>> m_properties;
 
+/*
+ * Copyright 2023 VeryGoodGraphics LTD <bd@verygoodgraphics.com>
+ *
+ * Licensed under the VGG License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.verygoodgraphics.com/licenses/LICENSE-1.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 public:
   static inline void handleGLError()
   {

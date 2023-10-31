24d04866ac990e868287ba7e0724bc20a01ab83d
Guanhua Hou <gaimanjing@gmail.com>
2023-10-31 13:12:39 +0800

    added BaseVggLayoutTestSuite

---
+++ test/domain/layout/base.hpp
@@ -0,0 +1,73 @@
+#include "Domain/Layout/Layout.hpp"
+#include "UseCase/StartRunning.hpp"
+
+#include "domain/model/daruma_helper.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace VGG;
+
+class BaseVggLayoutTestSuite : public ::testing::Test
+{
+protected:
+  std::shared_ptr<Layout::Layout> m_sut;
+
+  void SetUp() override
+  {
+  }
+
+  void TearDown() override
+  {
+  }
+
+  auto setupModel(const char* fileDir)
+  {
+    std::shared_ptr<Daruma> daruma{ new Daruma(Helper::RawJsonDocumentBuilder,
+                                               Helper::RawJsonDocumentBuilder) };
+    daruma->load(fileDir);
+    return daruma;
+  }
+
+  void setup(const char* fileDir)
+  {
+    auto daruma = setupModel(fileDir);
+    m_sut.reset(new Layout::Layout{ daruma->designDoc(), daruma->layoutDoc() });
+  }
+
+  void setupWithExpanding(const char* fileDir)
+  {
+    auto daruma = setupModel(fileDir);
+
+    StartRunning startRunning{ daruma };
+    m_sut = startRunning.layout();
+  }
+
+  auto firstPage()
+  {
+    auto tree = m_sut->layoutTree();
+    return tree->children()[0];
+  }
+
+  auto childFrame(int index)
+  {
+    auto frame = firstPage()->children()[index]->frame();
+    return frame;
+  }
+
+  auto grandsonFrame(int index, int index2)
+  {
+    auto frame = firstPage()->children()[index]->children()[index2]->frame();
+    return frame;
+  }
+
+  auto descendantFrame(std::vector<int> indexs)
+  {
+    auto node = firstPage();
+    for (auto index : indexs)
+    {
+      node = node->children()[index];
+    }
+
+    return node->frame();
+  }
+};

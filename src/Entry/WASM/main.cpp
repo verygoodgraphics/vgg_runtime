c6844445878ab95ec0a06c0bc3d5ecd095f900ee
Chaoya Li <harry75369@users.noreply.github.com>
2023-11-20 15:57:23 +0800

    Bugfix wasm failing to run (#144)
    * increased wasm initial memory
    
    * increased wasm stack size
    
    * fixed memory error when running vgg wasm version
    
    ---------
    
    Co-authored-by: Guanhua Hou <gaimanjing@gmail.com>

--- src/Entry/WASM/main.cpp
+++ src/Entry/WASM/main.cpp
@@ -58,7 +58,7 @@ extern "C"
     cfg.appName = "SdlRuntime";
     cfg.windowSize[0] = width;
     cfg.windowSize[1] = height;
-    cfg.graphicsContextConfig.multiSample = 4;
+    cfg.graphicsContextConfig.multiSample = 1;
     cfg.graphicsContextConfig.stencilBit = 8;
 
     auto app = application();

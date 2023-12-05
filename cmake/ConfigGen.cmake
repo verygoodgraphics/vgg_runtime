770b475345c553e2fe37a09fa63dd9d3303a2275
Shuoliu Yang <visysl@outlook.com>
2023-12-05 14:20:36 +0800

    add config header generation

---
+++ cmake/ConfigGen.cmake
@@ -0,0 +1,7 @@
+# generate header for VGGVersion.h.in
+file(READ ${CMAKE_SOURCE_DIR}/config/globalConstant.json GLOBAL_CONSTANT)
+string(JSON version GET ${GLOBAL_CONSTANT} formatVersion)
+message(STATUS "Parse for version: " ${version} ${GLOBAL_CONSTANT})
+set(VGG_PARSE_FORMAT_VER_STR ${version})
+configure_file(${CMAKE_SOURCE_DIR}/cmake/configure.in/VGGVersion.h.in VGGVersion_generated.h @ONLY)
+

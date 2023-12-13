00acccabfe744c40b7a6388218c4e24a83682583
Shuoliu Yang <visysl@outlook.com>
2023-12-13 16:31:04 +0800

    rename file

--- cmake/ConfigGen.cmake
+++ cmake/ConfigGen.cmake
@@ -1,5 +1,5 @@
 # generate header for VGGVersion.h.in
-file(READ ${CMAKE_SOURCE_DIR}/config/globalConstant.json GLOBAL_CONSTANT)
+file(READ ${CMAKE_SOURCE_DIR}/config/GlobalConstant.json GLOBAL_CONSTANT)
 string(JSON version GET ${GLOBAL_CONSTANT} formatVersion)
 message(STATUS "Parse for version: " ${version} ${GLOBAL_CONSTANT})
 set(VGG_PARSE_FORMAT_VER_STR ${version})

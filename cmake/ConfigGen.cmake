41f8b9e6156dd93fe130ff0f8870948f891cf91a
Chaoya Li <harry75369@gmail.com>
2023-12-14 14:27:55 +0800

    corrected usage of cmake string(JSON)

--- cmake/ConfigGen.cmake
+++ cmake/ConfigGen.cmake
@@ -1,7 +1,12 @@
-# generate header for VGGVersion.h.in
+# read format version from config
 file(READ ${CMAKE_SOURCE_DIR}/config/GlobalConstant.json GLOBAL_CONSTANT)
-string(JSON version GET ${GLOBAL_CONSTANT} formatVersion)
-message(STATUS "Parse for version: " ${version} ${GLOBAL_CONSTANT})
+string(JSON version ERROR_VARIABLE version_NOTFOUND GET ${GLOBAL_CONSTANT} formatVersion)
+if (version_NOTFOUND)
+  message(FATAL_ERROR "Format version nout found in config/GlobalConstant.json")
+endif()
+message(STATUS "Parse for format version: " ${version})
+
+# generate header for VGGVersion.h.in
 set(VGG_PARSE_FORMAT_VER_STR ${version})
 configure_file(${CMAKE_SOURCE_DIR}/cmake/configure.in/VGGVersion.h.in VGGVersion_generated.h @ONLY)
 

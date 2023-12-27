492f992b3469c034f8ead756571c5bfc1b2b8f13
Shuoliu Yang <visysl@outlook.com>
2023-12-27 15:36:22 +0800

    fixup gradient

--- src/Layer/VSkia.hpp
+++ src/Layer/VSkia.hpp
@@ -212,6 +212,40 @@ inline sk_sp<SkImage> loadImage(const std::string& imageGUID, const ResourceRepo
   return image;
 }
 
+inline SkMatrix makeMatrix(
+  const Bound&                          bound,
+  const glm::vec2&                      f,
+  const glm::vec2&                      t,
+  const std::variant<float, glm::vec2>& ellipse)
+{
+  const auto from = bound.map(bound.size() * f);
+  const auto to = bound.map(bound.size() * t);
+
+  auto theta = [](const glm::vec2& from, const glm::vec2& to)
+  {
+    const auto d = to - from;
+    return std::atan2(d.y, d.x);
+  };
+
+  auto ratio = 1.f;
+  std::visit(
+    Overloaded{ [&](const float& f) { ratio = f; },
+                [&](const glm::vec2& p)
+                {
+                  auto       pp = bound.map(bound.size() * p);
+                  const auto a = glm::distance(from, to);
+                  const auto b = glm::distance(from, pp);
+                  ratio = (a == 0.f) ? 1.f : b / a;
+                } },
+    ellipse);
+  SkMatrix mat = SkMatrix::I();
+  mat.postTranslate(-from.x, -from.y);
+  mat.postScale(1.0, ratio);
+  mat.postRotate(rad2deg(theta(from, to)));
+  mat.postTranslate(from.x, from.y);
+  return mat;
+}
+
 inline sk_sp<SkShader> makeFitPattern(const Bound& bound, const PatternFit& p)
 {
   auto img = loadImage(p.guid, Scene::getResRepo());
@@ -379,12 +413,10 @@ inline sk_sp<SkShader> makeGradientRadial(const Bound& bound, const G& g)
   auto minPosition = g.stops.front().position;
   auto maxPosition = g.stops.back().position;
 
-  auto f = bound.map(bound.size() * g.from);
-  auto t = bound.map(bound.size() * g.to);
-  auto start = glm::mix(f, t, minPosition);
-  auto end = glm::mix(f, t, maxPosition);
-
-  SkPoint  center{ (SkScalar)start.x, (SkScalar)start.y };
+  auto     f = bound.map(bound.size() * g.from);
+  auto     t = bound.map(bound.size() * g.to);
+  auto     start = glm::mix(f, t, minPosition);
+  auto     end = glm::mix(f, t, maxPosition);
   SkScalar r = glm::distance(end, start);
 
   std::vector<SkColor>  colors;
@@ -395,33 +427,9 @@ inline sk_sp<SkShader> makeGradientRadial(const Bound& bound, const G& g)
     auto p = it->position;
     positions.push_back((p - minPosition) / (maxPosition - minPosition));
   }
-
-  auto theta = [](const glm::vec2& from, const glm::vec2& to)
-  {
-    const auto d = to - from;
-    return vectorSign({ 1.0f, 0.f }, d) * std::atan2(d.y, d.x);
-  };
-  SkMatrix mat;
-  if (auto p = std::get_if<float>(&g.ellipse); p)
-  {
-    mat.postTranslate(-start.x, -start.y);
-    mat.postScale(*p, 1.0);
-    mat.postRotate(rad2deg(theta(f, t)));
-    mat.postTranslate(start.x, start.y);
-  }
-  else if (auto p = std::get_if<glm::vec2>(&g.ellipse); p)
-  {
-    auto pp = bound.map(bound.size() * (*p));
-    mat.postTranslate(-start.x, -start.y);
-    const auto a = glm::distance(f, t);
-    const auto b = glm::distance(f, pp);
-    const auto ratio = (a == 0.f) ? 0.f : b / a;
-    mat.postScale(ratio, 1.0);
-    mat.postRotate(rad2deg(theta(f, t)));
-    mat.postTranslate(start.x, start.y);
-  }
+  auto mat = makeMatrix(bound, g.from, g.to, g.ellipse);
   return SkGradientShader::MakeRadial(
-    center,
+    { start.x, start.y },
     r,
     colors.data(),
     positions.data(),
@@ -437,24 +445,14 @@ inline sk_sp<SkShader> makeGradientAngular(const Bound& bound, const G& g)
   if (g.stops.empty())
     return nullptr;
 
-  const auto minPosition = g.stops.front().position;
-  const auto maxPosition = g.stops.back().position;
-
-  const glm::vec2 from = { g.from[0], g.from[1] };
-  const glm::vec2 to = { g.to[0], g.to[1] };
-
-  auto f = bound.map(bound.size() * from);
-  auto t = bound.map(bound.size() * to);
-
-  auto center = f;
-
+  const auto            minPosition = g.stops.front().position;
+  const auto            maxPosition = g.stops.back().position;
+  auto                  minPosColor = g.stops.front().color;
+  auto                  maxPosColor = g.stops.back().color;
   std::vector<SkColor>  colors;
   std::vector<SkScalar> positions;
+  size_t                sz = g.stops.size();
 
-  auto minPosColor = g.stops.front().color;
-  auto maxPosColor = g.stops.back().color;
-
-  size_t sz = g.stops.size();
   if (minPosition > 0)
   {
     auto c = lerp(minPosColor, maxPosColor, (float)minPosition / (minPosition + 1 - maxPosition));
@@ -475,40 +473,9 @@ inline sk_sp<SkShader> makeGradientAngular(const Bound& bound, const G& g)
     sz += 1;
   }
 
-  SkMatrix mat = SkMatrix::I();
-
-  auto theta = [](const glm::vec2& from, const glm::vec2& to)
-  {
-    const auto d = to - from;
-    return vectorSign({ 1.0f, 0.f }, d) * std::atan2(d.y, d.x);
-  };
-  // mat.setRotate(glm::degrees(theta(f, t)), center.x, center.y);
-  if (auto p = std::get_if<float>(&g.ellipse); p)
-  {
-    mat.postTranslate(-center.x, -center.y);
-    mat.postScale(*p, 1.0);
-    mat.postRotate(-rad2deg(theta(f, t)));
-    mat.postTranslate(center.x, center.y);
-  }
-  else if (auto p = std::get_if<glm::vec2>(&g.ellipse); p)
-  {
-    auto pp = bound.map(bound.size() * (*p));
-    mat.postTranslate(-center.x, -center.y);
-    // const auto a = glm::distance(f, t);
-    // const auto b = glm::distance(f, pp);
-    // const auto ratio = (a == 0.f) ? 0.f : b / a;
-    mat.postScale(1.0, -vectorSign(pp - f, t - f));
-    mat.postRotate(-rad2deg(theta(f, t)));
-    mat.postTranslate(center.x, center.y);
-  }
-  return SkGradientShader::MakeSweep(
-    center.x,
-    center.y,
-    colors.data(),
-    positions.data(),
-    sz,
-    0,
-    &mat);
+  const auto mat = makeMatrix(bound, g.from, g.to, g.ellipse);
+  const auto from = bound.map(bound.size() * g.from);
+  return SkGradientShader::MakeSweep(from.x, from.y, colors.data(), positions.data(), sz, 0, &mat);
 }
 
 inline sk_sp<SkShader> makeGradientShader(const Bound& bound, const Gradient& gradient)

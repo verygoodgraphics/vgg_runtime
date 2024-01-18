f831e810721f011be2a2d4535f27722f6228fd55
Chaoya Li <harry75369@gmail.com>
2024-01-18 20:26:06 +0800

    upgraded serve_wasm script to use python3

--- serve_wasm.py
+++ serve_wasm.py
@@ -1,11 +1,25 @@
-import BaseHTTPServer, SimpleHTTPServer, sys, webbrowser as b
+#!/usr/bin/env python3
+
+import http.server
+import socketserver
+import sys
+import webbrowser as b
+
 HOST, PORT = ('localhost', 8888)
-class RequestHandler (SimpleHTTPServer.SimpleHTTPRequestHandler):
-    def end_headers (self):
+
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
         self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
-        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+        super().end_headers()
+
 RequestHandler.extensions_map['.wasm'] = 'application/wasm'
-if len(sys.argv) > 1: b.open_new_tab('http://%s:%d/%s' % (HOST, PORT, sys.argv[1]))
-BaseHTTPServer.HTTPServer((HOST, PORT), RequestHandler).serve_forever()
+
+if len(sys.argv) > 1:
+    b.open_new_tab('http://%s:%d/%s' % (HOST, PORT, sys.argv[1]))
+
+print('Listening on: http://%s:%d/' % (HOST, PORT))
+
+httpd = socketserver.TCPServer((HOST, PORT), RequestHandler)
+httpd.serve_forever()

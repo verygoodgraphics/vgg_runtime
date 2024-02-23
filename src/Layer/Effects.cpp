1f191963253f079b07fe816fd85a8a4b6b59b244
Shuoliu Yang <visysl@outlook.com>
2024-02-23 15:17:35 +0800

    support hue for image filter

--- src/Layer/Effects.cpp
+++ src/Layer/Effects.cpp
@@ -89,6 +89,36 @@ uniform float tint;
 uniform float hue;
 uniform float3 tintColor1;
 uniform float3 tintColor2;
+vec4 changeHue(vec4 color, float H){
+    const vec4  kRGBToYPrime = vec4 (0.299, 0.587, 0.114, 0.0);
+    const vec4  kRGBToI     = vec4 (0.596, -0.275, -0.321, 0.0);
+    const vec4  kRGBToQ     = vec4 (0.212, -0.523, 0.311, 0.0);
+    const vec4  kYIQToR   = vec4 (1.0, 0.956, 0.621, 0.0);
+    const vec4  kYIQToG   = vec4 (1.0, -0.272, -0.647, 0.0);
+    const vec4  kYIQToB   = vec4 (1.0, -1.107, 1.704, 0.0);
+    // Convert to YIQ
+    float   YPrime  = dot (color, kRGBToYPrime);
+    float   I      = dot (color, kRGBToI);
+    float   Q      = dot (color, kRGBToQ);
+
+    // Calculate the hue and chroma
+    float   hue     = atan (Q, I);
+    float   chroma  = sqrt (I * I + Q * Q);
+
+    // Make the user's adjustments
+    hue += H * 3.1415926535;
+
+    // Convert back to YIQ
+    Q = chroma * sin (hue);
+    I = chroma * cos (hue);
+
+    // Convert back to RGB
+    vec4    yIQ   = vec4 (YPrime, I, Q, 0.0);
+    color.r = dot (yIQ, kYIQToR);
+    color.g = dot (yIQ, kYIQToG);
+    color.b = dot (yIQ, kYIQToB);
+    return color;
+}
 
 mat4 brightnessMatrix( float brightness )
 {
@@ -172,6 +202,7 @@ vec4 main(vec4 inColor){
                  temperatureMatrix(temperature * 0.25) *
                  tintMatrix(tint, tintColor1, tintColor2) *
                  vec4(highlightAndShadow(inColor.rgb),1.0);
+    color =  changeHue(color, -hue);
     return vec4(color.rgb, inColor.a);
 }
 )";

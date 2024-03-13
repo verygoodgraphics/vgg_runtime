ccfd0bc5829cb3d67135eb50e51a46a853b6ff17
Guanhua Hou <gaimanjing@gmail.com>
2024-03-13 15:06:04 +0800

    added DesignModel test

---
+++ test/domain/model/DesignModelTests.cpp
@@ -0,0 +1,26 @@
+#include "daruma_helper.hpp"
+#include "test_config.hpp"
+
+#include "Domain/Model/DesignModel.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace VGG::Model;
+
+class DesignModelTestSuite : public ::testing::Test
+{
+};
+
+TEST_F(DesignModelTestSuite, Smoke)
+{
+  SKIP_LOCAL_TEST;
+
+  std::string filePath = "testDataDir/_local/vgg/design.json";
+  auto        designJson = Helper::load_json(filePath);
+
+  DesignModel data = designJson;
+  EXPECT_EQ(data.frames.size(), 1);
+
+  nlohmann::json json = data;
+  EXPECT_EQ(json["frames"].size(), 1);
+}
--- test/test_config.hpp
+++ test/test_config.hpp
@@ -5,5 +5,6 @@
 #define SKIP_S3_DEPENDENT_TEST GTEST_SKIP() << "Skipping local http url test";
 #define SKIP_SLOW_TEST GTEST_SKIP() << "Skipping slow test";
 #define SKIP_DEBUG_TEST GTEST_SKIP() << "Skipping debug test";
+#define SKIP_LOCAL_TEST GTEST_SKIP() << "Skipping test that requires local files";
 
 constexpr auto design_doc_schema_file = "./testDataDir/vgg-format-1.json";

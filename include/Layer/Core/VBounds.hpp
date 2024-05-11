38a9918e256deb79f379010bbeab23471c8f7e75
Shuoliu Yang <visysl@outlook.com>
2024-05-11 15:01:44 +0800

    fixup bounds transform

--- include/Layer/Core/VBounds.hpp
+++ include/Layer/Core/VBounds.hpp
@@ -141,6 +141,19 @@ public:
     return Bounds{ *minX, *minY, *maxX - *minX, *maxY - *minY };
   }
 
+  Bounds map(const glm::mat3& mat) const
+  {
+    auto p1 = mat * glm::vec3{ m_topLeft, 1.0 };
+    auto p2 = mat * glm::vec3{ glm::vec2{ m_bottomRight.x, m_topLeft.y }, 1.0 };
+    auto p3 = mat * glm::vec3{ m_bottomRight, 1.0 };
+    auto p4 = mat * glm::vec3{ glm::vec2{ m_topLeft.x, m_bottomRight.y }, 1.0 };
+    auto a = std::initializer_list<float>{ p1.x, p2.x, p3.x, p4.x };
+    auto b = std::initializer_list<float>{ p1.y, p2.y, p3.y, p4.y };
+    const auto [minX, maxX] = std::minmax_element(a.begin(), a.end());
+    const auto [minY, maxY] = std::minmax_element(b.begin(), b.end());
+    return Bounds{ *minX, *minY, *maxX - *minX, *maxY - *minY };
+  }
+
   // Bounds operator*(const layer::Transform& transform) const
   // {
   //   return this->transform(transform);
